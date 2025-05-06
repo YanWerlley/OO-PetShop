0799f20de1e4f866706a1d4494ad66a628f7ad80
Yan Werlley <yanwerlley0101@gmail.com>
2025-05-06 15:31:20 -0300

    feat(infra): implementação do controlador Hello World

---
+++ backend/src/main/java/com/petshop/api/PetShopApplication.java
@@ -0,0 +1,16 @@
+package com.petshop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe principal da aplicação Spring Boot
+ * @author Yan Werlley
+ */
+@SpringBootApplication
+public class PetShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetShopApplication.class, args);
+    }
+}
